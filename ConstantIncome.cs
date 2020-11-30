cac5603608a9cf9eb195863a6cfec2471e4ebdc8
Teodor Todorov <67126808+Teodor-Todorv@users.noreply.github.com>
2020-11-30 16:30:03 +0200

    Added 4 forms
    Income
    ConstantIncome
    Cost
    ConstantCost

---
+++ ConstantIncome.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FamilyBudget
+{
+    public partial class ConstantIncome : Form
+    {
+        BudgetManager bm = new BudgetManager();
+        public ConstantIncome()
+        {
+            InitializeComponent();
+        }
+
+        private void AddBT_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(DescriptionTB.Text) && string.IsNullOrEmpty(ValueTB.Text))
+            {
+                MessageBox.Show("You need to fill the boxes!");
+            }
+            else
+            {
+                float value = float.Parse(ValueTB.Text);
+                string date = this.dateTimePicker1.Value.ToString();
+                string description = DescriptionTB.Text;
+                bm.AddConstantIncome(date, description, value);
+                MessageBox.Show("ConstantIncome is added!");
+                DescriptionTB.Text = "";
+                ValueTB.Text = "";
+            }
+        }
+
+        private void ClearBT_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(DescriptionTB.Text) && string.IsNullOrEmpty(ValueTB.Text))
+            {
+                MessageBox.Show("Boxes are already empty!");
+            }
+            else
+            {
+                DescriptionTB.Text = "";
+                ValueTB.Text = "";
+            }
+
+        }
+        private void ValueTB_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char ch = e.KeyChar;
+            if (ch == 46 && ValueTB.Text.IndexOf('.') > -1)
+            {
+                e.Handled = true;
+                return;
+            }
+            if (!Char.IsDigit(ch) && !Char.IsControl(ch) && ch != 8 && ch != 46)
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace FamilyBudget
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new ConstantIncome());
         }
     }
 }
